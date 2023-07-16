4857f21e2fa64c1dd1f6e442d7c69f1dc0545103
Ahd libya <97837394+AhdLibya@users.noreply.github.com>
2023-07-16 15:29:33 +0300

    code style & add function::find_first_child_with_attribute add function::find_first_ancestor_with_attribute

--- src/instance.lua
+++ src/instance.lua
@@ -5,7 +5,7 @@ local function ClearInstance(ts: number , p_Instance: Instance)
 	end)
 end
 
-local function parsePath(pathStr)
+local function parse_path(pathStr: string)
 	local pathArray = string.split(pathStr, "/")
 	for idx = #pathArray, 1, -1 do
 		if pathArray[idx] == "" then
@@ -15,24 +15,24 @@ local function parsePath(pathStr)
 	return pathArray
 end
 
-local function GetValue(path, root)
+local function get_value(path: string, root: Instance?)
 	root = root or game
-	local instance = parsePath(path)
+	local instance = parse_path(path)
 	local first, last = instance[1], instance[#instance]
 	if first == last then return root[last] end
 	local nextRoot = root[first]
 	if nextRoot == nil then return nil end
-	return GetValue(table.concat(instance, "/", 2), nextRoot)
+	return get_value(table.concat(instance, "/", 2), nextRoot)
 end
 
-local function Import(filePath)
-	local module = GetValue(filePath)
+local function import(filePath: string)
+	local module = get_value(filePath)
 	if typeof(module) == "Instance" and module:IsA("ModuleScript") then return require(module) end
 	error(`{filePath} is not valid`)
 end
 
 
-local function DisconnectEvents(Listenr: RBXScriptConnection | {RBXScriptConnection})
+local function disconnect_events(Listenr: RBXScriptConnection | {RBXScriptConnection})
 	if typeof(Listenr) == "RBXScriptConnection" then
 		Listenr:Disconnect()
 	elseif typeof(Listenr) == "table" then
@@ -72,13 +72,35 @@ local function for_children(parent: Instance , callback : (instance: Instance) -
 	end
 end
 
+local function find_first_child_with_attribute(instance: Instance , attribute: string , recursive: boolean?)
+	local children = recursive and instance:GetDescendants() or instance:GetChildren()
+	for _ , child: Instance in children do
+		if child:GetAttribute(attribute) ~= nil then return child end
+	end
+	return nil
+end
+
+local function find_first_ancestor_with_attribute(instance: Instance , attribute: string)
+	local parent = instance.Parent
+	local child = nil
+	while parent ~= nil and not child do
+		if parent:GetAttribute(attribute) ~= nil then
+			child = parent
+		else
+			parent = parent.Parent
+		end
+	end
+	return child
+end
 
 return {
-	clear_instance 			= ClearInstance;
-	get_value				= GetValue;
-	import 					= Import;
-	disconnect_events		= DisconnectEvents;
-	load_modules 			= load_modules;
-	find_or_create_folder 	= find_or_create_folder;
-	for_children 			= for_children;
+    clear_instance          = ClearInstance;
+    get_value               = get_value;
+    import                  = import;
+    disconnect_events       = disconnect_events;
+    load_modules            = load_modules;
+    find_or_create_folder   = find_or_create_folder;
+    for_children            = for_children;
+    find_first_child_with_attribute = find_first_child_with_attribute;
+    find_first_ancestor_with_attribute = find_first_ancestor_with_attribute;
 }
